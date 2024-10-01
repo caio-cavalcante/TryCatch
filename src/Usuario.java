c916537c9eb663e00f799c0893fa57ed180e610d
caio <caio.cav.ara@gmail.com>
2024-10-01 16:43:43 -0300

    Try Catch

---
+++ src/Usuario.java
@@ -0,0 +1,18 @@
+public class Usuario {
+    private String nome;
+    private int idade;
+
+    public void setNome(String nome) throws NomeInvalidoException {
+        if (nome == null || nome.isEmpty()) {
+            throw new NomeInvalidoException("Escreva um nome válido.");
+        }
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) throws IdadeInvalidaException{
+        if (idade < 18 || idade > 100) {
+            throw new IdadeInvalidaException("Escreva uma idade válida.");
+        }
+        this.idade = idade;
+    }
+}
